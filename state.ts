bca60819b985a1a351c7f8dcb7efdb7955f4df52
Karan Handa <karan.handa.fox@gmail.com>
2026-04-13 15:54:37 +0530

    feat: define unconditionals and fix types

--- state.ts
+++ state.ts
@@ -1,45 +1,25 @@
-type SimulationState = {
+export type SimulationState = {
   config: SimulationConfig;
   clock: SimulationClock;
-  frames: SimFrame[];
-  packets: PacketEvent[];
-  aggregates: NetworkAggregates;
-  ui: UiState;
+  mcsTable: readonly McsTableEntry[];
+  physicalConstants: PhysicalConstants;
+  hardwareNominalConstants: HardwareNominalConstants;
+  frames: readonly IndependentSimulationFrame[];
 };
 
-type SimulationConfig = {
+export type SimulationConfig = {
   tle: TleElements;
-
-  // TODO: Will be taken from UI, default will be set
-  groundStation: {
-    name: string;
-    latDeg: number;
-    lonDeg: number;
-    altitudeM: number;
-    minElevationDeg: number;
-  };
-
+  groundStation: GroundStationConfig;
   radio: {
     downlink: RadioLinkConfig;
     uplink: RadioLinkConfig;
   };
-
-  traffic: {
-    offeredLoadMbps: number;
-    packetSizeBytes: number;
-    maxRetransmissions: number;
-    retransmissionDelayMs: number;
-  };
-
-  scenario: {
-    seed: number;
-    enabledFaults: FaultConfig[];
-  };
+  traffic: TrafficConfig;
+  scenario: ScenarioConfig;
 };
 
-type TleElements = {
+export type TleElements = {
   name: string;
-
   satelliteCatalogNumber: number;
   classification: "U" | "C" | "S";
   internationalDesignator: {
@@ -52,14 +32,12 @@ type TleElements = {
     dayOfYear: number;
     fractionalDay: number;
   };
-
   meanMotionFirstDerivative: number;
   meanMotionSecondDerivative: number;
   bstarDragTerm: number;
   ephemerisType: number;
   elementSetNumber: number;
   line1Checksum: number;
-
   inclinationDeg: number;
   rightAscensionAscendingNodeDeg: number;
   eccentricity: number;
@@ -70,228 +48,313 @@ type TleElements = {
   line2Checksum: number;
 };
 
-type RadioLinkConfig = {
+export type GroundStationConfig = {
+  name: string;
+  latDeg: number;
+  lonDeg: number;
+  altitudeM: number;
+  minElevationDeg: number;
+};
+
+export type RadioLinkConfig = {
   carrierHz: number;
   bandwidthHz: number;
-  polarization: "RHCP"
-}
+  polarization: "RHCP";
+};
 
-type SimulationClock = {
+export type TrafficConfig = {
+  offeredLoadMbps: number;
+  packetSizeBytes: number;
+  maxRetransmissions: number;
+  retransmissionDelayMs: number;
+};
+
+export type ScenarioConfig = {
+  seed: number;
+  enabledFaults: readonly FaultConfig[];
+};
+
+// TODO: These should be stochastically determined based on sun position / other factors. Also should be able to introduce a fault and see downstream effects of that
+export type FaultConfig = {
+  id: string;
+  label: string;
+  startOffsetSeconds: number;
+  durationSeconds: number;
+};
+
+export type SimulationClock = {
   startUnixMs: number;
   endUnixMs: number;
-  stepSeconds: number; // probably 1
-  currentFrameIndex: number;
-  playbackRate: number;
-  isPlaying: boolean;
+  stepSeconds: number;
 };
 
-type SimFrame = {
-  tUnixMs: number;
-  index: number;
-
-  geometry: GeometryState;
-  satellite: SatelliteState;
-  ground: GroundStationState;
-  link: LinkState;
-  network: NetworkFrameState;
-
-  causes: CauseTag[];
+export type McsTableEntry = {
+  id: string;
+  modulation: "BPSK" | "QPSK" | "8PSK" | "16QAM" | "64QAM";
+  codingRate: string;
+  spectralEfficiencyBitsPerHz: number;
+  minSnrDb: number;
 };
 
-type GeometryState = {
-  satelliteEciKm: Vec3;
-  satelliteEcefKm: Vec3;
-
-  groundAzimuthDeg: number;
-  groundElevationDeg: number;
-  rangeKm: number;
-  rangeRateMps: number;
-
-  dopplerHz: number;
-  propagationDelayMs: number;
-
-  visible: boolean;
-
-  satelliteAntennaAzDeg: number;
-  satelliteAntennaElDeg: number;
-  scanAngleDeg: number;
-  offBoresightDeg: number;
-  withinFieldOfRegard: boolean;
-};
-
-type SatelliteState = {
-  attitude: AttitudeState;
-  array: PhasedArrayState;
-  rfFrontend: RfFrontendState;
-  powerAmplifier: PowerAmplifierState;
-  oscillator: OscillatorState;
-  thermal: ThermalState;
-  powerBus: PowerBusState;
-  compute: ComputeState;
-  faults: FaultState;
-
-  derivedRadio: DerivedRadioState;
-};
-
-type PhasedArrayState = {
-  totalElements: number;
-  activeElements: number;
-  degradedElements: number;
-  failedElements: number;
-
-  commandedScanAngleDeg: number;
-  scanLossDb: number;
-  elementFailureLossDb: number;
-  phaseErrorDegRms: number;
-
-  arrayGainDbi: number;
-  beamwidthDeg: number;
-  beamQuality: number; // 0..1
-  polarizationLossDb: number;
-};
-
-type PowerAmplifierState = {
-  commandedPowerDbw: number;
-  maxPowerDbw: number;
-  actualOutputPowerDbw: number;
-
-  backoffDb: number;
-  compressionDb: number;
-  distortionEvmPercent: number;
-
-  efficiency: number;
-  powerDrawW: number;
-  temperatureC: number;
-  thermalDeratingDb: number;
-};
-
-type OscillatorState = {
-  nominalFrequencyHz: number;
-  frequencyOffsetHz: number;
-  driftHzPerSecond: number;
-  temperatureSensitivityHzPerC: number;
-
-  phaseNoiseDbc: number;
-  timingJitterNs: number;
-
-  dopplerCorrectionHz: number;
-  dopplerResidualHz: number;
-};
-
-type ThermalState = {
-  arrayTempC: number;
-  paTempC: number;
-  rfTempC: number;
-  oscillatorTempC: number;
-  computeTempC: number;
-
-  thermalThrottleActive: boolean;
-  thermalDeratingDb: number;
-};
-
-type PowerBusState = {
-  solarGenerationW: number;
-  batterySocPercent: number;
-  busLimitW: number;
-
-  radioPowerW: number;
-  computePowerW: number;
-  thermalControlPowerW: number;
-  totalLoadW: number;
-
-  powerMarginW: number;
-  loadSheddingActive: boolean;
-  txPowerCapDbw: number;
-};
-
-type ComputeState = {
-  cpuUtilizationPercent: number;
-  beamformingLoadPercent: number;
-  modemLoadPercent: number;
-  routingLoadPercent: number;
-
-  schedulerLatencyMs: number;
-  packetQueueDepth: number;
-  missedDeadlines: number;
-
-  dopplerUpdateAgeMs: number;
-};
-
-type DerivedRadioState = {
-  txPowerDbw: number;
-  arrayGainDbi: number;
-  eirpDbw: number;
-
-  carrierFrequencyHz: number;
-  frequencyErrorHz: number;
-  dopplerResidualHz: number;
-
-  evmPercent: number;
-  phaseNoiseDbc: number;
-
-  pointingLossDb: number;
-  polarizationLossDb: number;
-  implementationLossDb: number;
-};
-
-type GroundStationState = {
-  antenna: {
-    gainDbi: number;
-    pointingErrorDeg: number;
-    pointingLossDb: number;
-    polarizationLossDb: number;
-    trackingLocked: boolean;
+export type PhysicalConstants = {
+  speedOfLightMps: number;
+  boltzmannConstantJPerK: number;
+  earthModel: {
+    name: "WGS84";
+    meanRadiusM: number;
+    equatorialRadiusM: number;
+    polarRadiusM: number;
+    flattening: number;
   };
+};
 
+export type HardwareNominalConstants = {
+  powerAmplifier: {
+    minOutputPowerDbw: number;
+    nominalOutputPowerDbw: number;
+    maxOutputPowerDbw: number;
+    nominalBackoffDb: number;
+  };
+  phasedArray: {
+    defaultElementCount: number;
+  };
   receiver: {
-    bandwidthHz: number;
-    noiseFigureDb: number;
-    systemNoiseTempK: number;
-    implementationLossDb: number;
-    frequencyTrackingToleranceHz: number;
-  };
-
-  weather: {
-    rainLossDb: number;
-    atmosphericLossDb: number;
+    defaultNoiseFigureDb: number;
   };
 };
 
-type LinkState = {
-  freeSpacePathLossDb: number;
-  atmosphericLossDb: number;
-  rainLossDb: number;
-
-  receivedPowerDbm: number;
-  noisePowerDbm: number;
-
-  snrDb: number;
-  effectiveSnrDb: number;
-
-  dopplerResidualHz: number;
-  evmPercent: number;
-
-  selectedMcs: McsMode;
-  ber: number;
-  packetErrorRate: number;
-
-  linkAvailable: boolean;
-  linkMarginDb: number;
+export type IndependentSimulationFrame = {
+  index: number;
+  tUnixMs: number;
+  clock: {
+    startUnixMs: number;
+    endUnixMs: number;
+    stepSeconds: number;
+    currentUnixMs: number;
+  };
+  config: SimulationConfig;
+  mcsTable: readonly McsTableEntry[];
+  physicalConstants: PhysicalConstants;
+  hardwareNominalConstants: HardwareNominalConstants;
 };
 
-type NetworkFrameState = {
-  offeredLoadMbps: number;
-  physicalRateMbps: number;
-  usableRateMbps: number;
-
-  packetsGenerated: number;
-  packetsAttempted: number;
-  packetsDelivered: number;
-  packetsLost: number;
-  packetsRetransmitted: number;
-
-  queueDepthPackets: number;
-  meanLatencyMs: number;
-  jitterMs: number;
-  goodputMbps: number;
+export const PHYSICAL_CONSTANTS: PhysicalConstants = {
+  speedOfLightMps: 299_792_458,
+  boltzmannConstantJPerK: 1.380649e-23,
+  earthModel: {
+    name: "WGS84",
+    meanRadiusM: 6_371_008.8,
+    equatorialRadiusM: 6_378_137,
+    polarRadiusM: 6_356_752.314245,
+    flattening: 1 / 298.257223563,
+  },
 };
+
+export const HARDWARE_NOMINAL_CONSTANTS: HardwareNominalConstants = {
+  powerAmplifier: {
+    minOutputPowerDbw: -5,
+    nominalOutputPowerDbw: 6,
+    maxOutputPowerDbw: 9,
+    nominalBackoffDb: 2,
+  },
+  phasedArray: {
+    defaultElementCount: 256,
+  },
+  receiver: {
+    defaultNoiseFigureDb: 1.8,
+  },
+};
+
+export const MCS_TABLE: readonly McsTableEntry[] = [
+  {
+    id: "MCS-0",
+    modulation: "BPSK",
+    codingRate: "1/2",
+    spectralEfficiencyBitsPerHz: 0.5,
+    minSnrDb: 1.0,
+  },
+  {
+    id: "MCS-1",
+    modulation: "QPSK",
+    codingRate: "1/2",
+    spectralEfficiencyBitsPerHz: 1.0,
+    minSnrDb: 3.0,
+  },
+  {
+    id: "MCS-2",
+    modulation: "QPSK",
+    codingRate: "3/4",
+    spectralEfficiencyBitsPerHz: 1.5,
+    minSnrDb: 6.0,
+  },
+  {
+    id: "MCS-3",
+    modulation: "8PSK",
+    codingRate: "2/3",
+    spectralEfficiencyBitsPerHz: 2.0,
+    minSnrDb: 9.0,
+  },
+  {
+    id: "MCS-4",
+    modulation: "16QAM",
+    codingRate: "1/2",
+    spectralEfficiencyBitsPerHz: 2.0,
+    minSnrDb: 12.0,
+  },
+  {
+    id: "MCS-5",
+    modulation: "16QAM",
+    codingRate: "3/4",
+    spectralEfficiencyBitsPerHz: 3.0,
+    minSnrDb: 15.0,
+  },
+  {
+    id: "MCS-6",
+    modulation: "64QAM",
+    codingRate: "2/3",
+    spectralEfficiencyBitsPerHz: 4.0,
+    minSnrDb: 19.0,
+  },
+  {
+    id: "MCS-7",
+    modulation: "64QAM",
+    codingRate: "5/6",
+    spectralEfficiencyBitsPerHz: 5.0,
+    minSnrDb: 22.5,
+  },
+];
+
+export const DEFAULT_SIMULATION_CONFIG: SimulationConfig = {
+  // STARLINK-1008
+  // 1 44714U 19074B   26102.16021996  .00047530  00000+0  13979-2 0  9995
+  // 2 44714  53.1560  22.7027 0003306 151.7589 208.3593 15.34644574354005
+  tle: {
+    name: "STARLINK-1008",
+    satelliteCatalogNumber: 44714,
+    classification: "U",
+    internationalDesignator: {
+      launchYear: 2019,
+      launchNumberOfYear: 74,
+      launchPiece: "B",
+    },
+    epoch: {
+      year: 2026,
+      dayOfYear: 102,
+      fractionalDay: 0.16021996,
+    },
+    meanMotionFirstDerivative: 0.0004753,
+    meanMotionSecondDerivative: 0,
+    bstarDragTerm: 0.0013979,
+    ephemerisType: 0,
+    elementSetNumber: 999,
+    line1Checksum: 5,
+    inclinationDeg: 53.156,
+    rightAscensionAscendingNodeDeg: 22.7027,
+    eccentricity: 0.0003306,
+    argumentOfPerigeeDeg: 151.7589,
+    meanAnomalyDeg: 208.3593,
+    meanMotionRevsPerDay: 15.34644574,
+    revolutionNumberAtEpoch: 35400,
+    line2Checksum: 5,
+  },
+  groundStation: {
+    name: "Bengaluru Ground Station",
+    latDeg: 12.9716,
+    lonDeg: 77.5946,
+    altitudeM: 920,
+    minElevationDeg: 25,
+  },
+  radio: {
+    downlink: {
+      carrierHz: 18_200_000_000,
+      bandwidthHz: 250_000_000,
+      polarization: "RHCP",
+    },
+    uplink: {
+      carrierHz: 28_300_000_000,
+      bandwidthHz: 250_000_000,
+      polarization: "RHCP",
+    },
+  },
+  traffic: {
+    offeredLoadMbps: 200,
+    packetSizeBytes: 1200,
+    maxRetransmissions: 3,
+    retransmissionDelayMs: 100,
+  },
+  scenario: {
+    seed: 44714,
+    enabledFaults: [],
+  },
+};
+
+const TLE_EPOCH_UNIX_MS = tleEpochToUnixMs(DEFAULT_SIMULATION_CONFIG.tle.epoch);
+const DEFAULT_SIMULATION_DURATION_SECONDS = 180;
+
+export const DEFAULT_SIMULATION_CLOCK: SimulationClock = {
+  startUnixMs: TLE_EPOCH_UNIX_MS,
+  endUnixMs:
+    TLE_EPOCH_UNIX_MS + (DEFAULT_SIMULATION_DURATION_SECONDS - 1) * 1_000,
+  stepSeconds: 1,
+};
+
+export function buildIndependentSimulationFrames(
+  config: SimulationConfig,
+  clock: SimulationClock,
+  mcsTable: readonly McsTableEntry[] = MCS_TABLE,
+  physicalConstants: PhysicalConstants = PHYSICAL_CONSTANTS,
+  hardwareNominalConstants: HardwareNominalConstants = HARDWARE_NOMINAL_CONSTANTS,
+): IndependentSimulationFrame[] {
+  if (clock.stepSeconds <= 0) {
+    throw new Error("Simulation clock stepSeconds must be greater than zero.");
+  }
+
+  if (clock.endUnixMs < clock.startUnixMs) {
+    throw new Error("Simulation clock endUnixMs must be >= startUnixMs.");
+  }
+
+  const stepMs = clock.stepSeconds * 1_000;
+  const frames: IndependentSimulationFrame[] = [];
+
+  for (
+    let currentUnixMs = clock.startUnixMs, index = 0;
+    currentUnixMs <= clock.endUnixMs;
+    currentUnixMs += stepMs, index += 1
+  ) {
+    frames.push({
+      index,
+      tUnixMs: currentUnixMs,
+      clock: {
+        startUnixMs: clock.startUnixMs,
+        endUnixMs: clock.endUnixMs,
+        stepSeconds: clock.stepSeconds,
+        currentUnixMs,
+      },
+      config,
+      mcsTable,
+      physicalConstants,
+      hardwareNominalConstants,
+    });
+  }
+
+  return frames;
+}
+
+export const DEFAULT_SIMULATION_STATE: SimulationState = {
+  config: DEFAULT_SIMULATION_CONFIG,
+  clock: DEFAULT_SIMULATION_CLOCK,
+  mcsTable: MCS_TABLE,
+  physicalConstants: PHYSICAL_CONSTANTS,
+  hardwareNominalConstants: HARDWARE_NOMINAL_CONSTANTS,
+  frames: buildIndependentSimulationFrames(
+    DEFAULT_SIMULATION_CONFIG,
+    DEFAULT_SIMULATION_CLOCK,
+  ),
+};
+
+function tleEpochToUnixMs(epoch: TleElements["epoch"]): number {
+  const dayStartUnixMs = Date.UTC(epoch.year, 0, epoch.dayOfYear);
+  const fractionalDayMs = epoch.fractionalDay * 86_400_000;
+
+  return Math.round(dayStartUnixMs + fractionalDayMs);
+}
